6353f4bb522d3444b5cd80f19383240c1f27c5e8
Slawek <61154596+Ultran@users.noreply.github.com>
2021-09-03 22:49:38 +0200

    add leaflet

--- src/components/User.js
+++ src/components/User.js
@@ -3,6 +3,7 @@ import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,6 +24,7 @@ const User = ({ user, changeView, isListActive }) => {
   const { street, city } = user.location;
   const { email, registered, login } = user;
   const { medium } = user.picture;
+  const { latitude, longitude } = user.location.coordinates;
   const streetName = street.name ? street.name : "no data";
   const cityName = city ? city : "no data";
   const registerDate = registered.date.slice(0, 10);
@@ -39,50 +41,73 @@ const User = ({ user, changeView, isListActive }) => {
   };
 
   return (
-    <div className="userContainer">
-      <Paper style={{ padding: 10, width: 300 }}>
-        <div className="generalData">
-          <Avatar
-            style={{ marginRight: 20 }}
-            className={classes.large}
-            alt="Remy Sharp"
-            src={medium}
-          />
-          <ul>{nameFull(first, last)}</ul>
-        </div>
-        <ul>
-          {!isListActive && (
-            <>
-              <li>
-                <span>street</span>
-                {streetName} <span>city</span>
-                {cityName}
-              </li>
-              <li>
-                <span>email</span>
-                {email}
-              </li>
-              <li>
-                <span>registered</span>
-                {registerDate}
-              </li>
-            </>
-          )}
+    <>
+      <div className="userContainer">
+        <Paper style={{ padding: 10, width: 300 }}>
+          <div className="generalData">
+            <Avatar
+              style={{ marginRight: 20 }}
+              className={classes.large}
+              alt="Remy Sharp"
+              src={medium}
+            />
+            <ul>{nameFull(first, last)}</ul>
+          </div>
+          <ul>
+            {!isListActive && (
+              <>
+                <li>
+                  <span>street</span>
+                  {streetName} <span>city</span>
+                  {cityName}
+                </li>
+                <li>
+                  <span>email</span>
+                  {email}
+                </li>
+                <li>
+                  <span>registered</span>
+                  {registerDate}
+                </li>
+              </>
+            )}
 
-          <li className="button">
-            <Button
-              component={Link}
-              onClick={() => changeView()}
-              to={isListActive ? `/users/${login.uuid}` : "/"}
-              size="small"
-              variant="outlined"
-            >
-              {isListActive ? "details" : "back"}
-            </Button>
-          </li>
-        </ul>
-      </Paper>
-    </div>
+            <li className="button">
+              <Button
+                component={Link}
+                onClick={() => changeView()}
+                to={isListActive ? `/users/${login.uuid}` : "/"}
+                size="small"
+                variant="outlined"
+              >
+                {isListActive ? "details" : "back"}
+              </Button>
+            </li>
+          </ul>
+        </Paper>
+      </div>
+
+      {!isListActive && (
+        <div className="mapSection">
+          <MapContainer
+            className="mapContainer"
+            center={[parseInt(latitude), parseInt(longitude)]}
+            zoom={5}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[parseInt(latitude), parseInt(longitude)]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      )}
+    </>
   );
 };
 
